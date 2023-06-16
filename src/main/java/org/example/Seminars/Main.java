7ceb7cf1601ef102fb95d144757cc3ab96800468
DenisM <mdr2010@yandex.ru>
2023-06-17 02:46:49 +0500

    add Sem

--- src/main/java/org/example/Seminars/Main.java
+++ src/main/java/org/example/Seminars/Main.java
@@ -1,2 +1,16 @@
-package org.example.Seminars;public class Main {
+// ГОРЯЧИЕ КЛАВИШИ
+
+// psvm - быстрый набор public static void main(String[] args)
+// sout - быстрый набор для System.out.println()
+// cntrl + a выделяем класс
+// cntrl + Alt + l формат документа
+// cntrl + Alt + o удаление неиспользуемых библиотек
+// Alt + Enter наведенный на класс (Scanner) подтягивает import java.util.Scanner
+// /** Enter и пишем задание
+// fori + tab получаем for (int i = 0; i < ; i++) {}
+// count + .soutv выведет System.out.println("count = " + count)
+
+package org.example.Seminars;
+
+public class Main {
 }
