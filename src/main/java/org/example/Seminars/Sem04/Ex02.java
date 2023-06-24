6255ef17176ee600d315e7c71185d230205f649b
DenisM <mdr2010@yandex.ru>
2023-06-25 02:31:53 +0500

    add Sem04

--- src/main/java/org/example/Seminars/Sem04/Ex02.java
+++ src/main/java/org/example/Seminars/Sem04/Ex02.java
@@ -1,2 +1,26 @@
-package org.example.Seminars.Sem04;public class Ex02 {
+package org.example.Seminars.Sem04;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            linkedList.add(scanner.nextInt());
+        }
+        System.out.println("linkedList = " + linkedList);
+        scanner.close();
+        System.out.println("linkedList.peek() = " + linkedList.peek()); // возвращает список
+        System.out.println("linkedList.pop() = " + linkedList.pop()); // удаляет 1 значение
+        linkedList.push(1); // добавили 1 в начало
+        linkedList.add(2); // добавили 2 в конец
+        linkedList.addLast(3); // добавили 3 в конец
+        linkedList.addFirst(3); // добавили 3 в начало
+        linkedList.removeFirst(); // удаление первого значения
+        linkedList.removeLast(); // удаление последнего значения
+        linkedList.contains(3); // true - 3 есть в списке
+    }
 }
--- src/main/java/org/example/Seminars/Sem04/Ex03Random.java
+++ src/main/java/org/example/Seminars/Sem04/Ex03Random.java
@@ -1,2 +1,23 @@
-package org.example.Seminars.Sem04;public class Ex03Random {
+package org.example.Seminars.Sem04;
+
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex03Random {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        LinkedList<Integer> linkedList = createList(n);
+        System.out.println("linkedList = " + linkedList);
+    }
+
+    private static LinkedList<Integer> createList(int n) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            linkedList.add(random.nextInt(10));
+        }
+        return linkedList;
+    }
 }
